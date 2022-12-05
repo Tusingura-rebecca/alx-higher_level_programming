eef2a08f08a01887ae2d4873dd13b9a3dbe79206
Tusingura-rebecca <rtusingura@gmail.com>
2022-12-05 08:23:23 -0800

    task8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0] if length > 0 else "None"
-    my_tuple = length, first_char
-    return(my_tuple)
+    tup = length, first_char
+    return(tup)

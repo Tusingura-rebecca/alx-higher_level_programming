cdc92a306f9813f7bd19874ca8db6ec533218e94
Tusingura-rebecca <rtusingura@gmail.com>
2023-01-11 15:02:43 -0800

    inherit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""inheritance checker"""
+
+
 def inherits_from(obj, a_class):
     """ returns True if obj is an instance of a_class or a class inherited
     from a_class"""
-    return isinstance(obj, a_class) and not issubclass(a_class, type(obj))
+    return issubclass(obj.__class__, a_class)

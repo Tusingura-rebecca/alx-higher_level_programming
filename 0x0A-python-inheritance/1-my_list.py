6535d59116e8f3cc779f235496ccc673063955a6
Tusingura-rebecca <rtusingura@gmail.com>
2023-01-11 10:59:34 -0800

    my list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,9 +5,6 @@ defines class MyList that inherits from list
 
 
 class MyList(list):
-    """ initializes MyList """
-    def __init__(self):
-        pass
 
     """ prints sorted list """
     def print_sorted(self):

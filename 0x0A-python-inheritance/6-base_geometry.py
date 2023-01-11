b588ef100bbb08e45b7d402f79b5e6c7e53db1c5
Tusingura-rebecca <rtusingura@gmail.com>
2023-01-11 15:14:54 -0800

    base

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Base Geometry class empty"""
+
+    def area(self):
+        """Area output"""
+
+        raise Exception('area() is not implemented')

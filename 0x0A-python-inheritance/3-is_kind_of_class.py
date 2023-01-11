6049cdbb2d29833916fa7e02fd2ee818cc7aeae1
Tusingura-rebecca <rtusingura@gmail.com>
2023-01-11 14:53:07 -0800

    kind

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """
 Module that checks if an object is an instance of the specified class or
 if the object is an instance of a class inherited from the specified class

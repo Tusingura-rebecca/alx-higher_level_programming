f66ad821954fabbb94d7f7d346827ef49cf192da
Tusingura-rebecca <rtusingura@gmail.com>
2023-01-11 16:25:03 -0800

    retangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Imports class BaseGeometry
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+"""
+Defines class Rectangle
+"""
+
+
+class Rectangle(BaseGeometry):
+    """ initializes class """
+    def __init__(self, width, height):
+        if BaseGeometry.integer_validator(self, "width", width):
+            self.width = width
+        if BaseGeometry.integer_validator(self, "height", height):
+            self.height = height

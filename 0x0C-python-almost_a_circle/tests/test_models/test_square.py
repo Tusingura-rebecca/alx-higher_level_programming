47b728d0ebaed932ddcc36928d8917f35ec25bad
Tusingura-rebecca <rtusingura@gmail.com>
2023-01-18 22:46:53 -0800

    fixer

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -5,7 +5,7 @@ Each test has the number of the task,
 and the number of the test for that task
 (i.e 'test_17_0' for the first test of task 17)
 """
- 
+
 import unittest
 import io
 import contextlib

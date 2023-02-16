97faa881220acff738f23f1490de9f6b0442b52e
Tusingura-rebecca <rtusingura@gmail.com>
2023-02-16 04:13:26 -0800

    carlifornia

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+ -- 8-cities_of_california_subquery.sql
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;

1bfbc85e97e0180993e4215358d7dab6b45d3da6
Tusingura-rebecca <rtusingura@gmail.com>
2023-07-31 16:03:42 +0000

    characters

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,23 @@
 #!/usr/bin/node
+// Script that prints all characters of a Star Wars movie
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
+const args = process.argv.slice(1);
+const movie = 'https://swapi-api.hbtn.io/api/films/' + args[1];
+
+request(movie, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
     const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
+    for (const character of characters) {
+      request(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
           console.log(JSON.parse(body).name);
         }
       });
-    });
+    }
   }
 });

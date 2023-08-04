00d0eaedb3fd0f609f5c33c852f8851fa936187e
Tusingura-rebecca <rtusingura@gmail.com>
2023-08-04 13:49:41 +0000

    starwars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+const $ = window.$;
+$(function () {
+  $.get('https://swapi.co/api/films/?format=json', function (data, textStatus) {
+    if (textStatus === 'success') {
+      const films = data.results;
+      for (const i in films) {
+        $('#list_movies').append('<li>' + films[i].title + '</li>');
+      }
+    }
+  });
+});
